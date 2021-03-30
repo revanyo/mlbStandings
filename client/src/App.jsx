e9d5638bbd8ed5048cdf0be4bd2e5bc4ddd89dc3
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-03-29 22:34:57 -0600

    Simple API Request that pulls data and lists NL Teams by Division and wins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App(props) {
+  const options = {
+    method: "GET",
+    url: "https://api-baseball.p.rapidapi.com/standings",
+    params: { league: "1", season: "2020" },
+    headers: {
+      "x-rapidapi-key": "916506272cmshfc8573fdda60a55p133c1ajsnacc6487e2f26",
+      "x-rapidapi-host": "api-baseball.p.rapidapi.com",
+    },
+  };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.request(options).then((res) => {
+      setData(res.data.response[0]);
+    });
+  }, []);
+
+  let nlWest = data.filter((team) => team.group.name === "NL West");
+  let nlEast = data.filter((team) => team.group.name === "NL East");
+  let nlCentral = data.filter((team) => team.group.name === "NL Central");
+
+  console.log(nlCentral);
   return (
     <div>
-      <p>Hello</p>
+      <p>NL West</p>
+      <br></br>
+      {nlWest.map((team) => (
+        <p key={team.team.id}>
+          {team.team.name}
+          {team.games.win.total}
+        </p>
+      ))}
+      <p>NL Central</p>
+      <br></br>
+      {nlEast.map((team) => (
+        <p key={team.team.id}>
+          {team.team.name}
+          {team.games.win.total}
+        </p>
+      ))}
+      <p>NL East</p>
+      <br></br>
+      {nlCentral.map((team) => (
+        <p key={team.team.id}>
+          {team.team.name}
+          {team.games.win.total}
+        </p>
+      ))}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const unirest = require("unirest");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '..', '/public')));
+app.use(express.static(path.join(__dirname, "..", "/public")));
 app.use(express.json());
 
-app.listen(PORT, ()=>{
-	console.log('Listening on', PORT)
+const req = unirest("GET", "https://api-baseball.p.rapidapi.com/standings");
+
+app.get("/standings", (req, res) => {
+  res;
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on", PORT);
 });
